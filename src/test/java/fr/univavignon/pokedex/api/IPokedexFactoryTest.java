9876b4d25c1c0253459ec6003fd46d8e3be1f14e
hchakour2003 <hchakour2003@gmail.com>
2024-10-17 10:46:30 +0200

    Test de IPokedexFactoryTest

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,34 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokedexFactoryTest {
+
+    IPokedexFactory pokedexFactory;
+    IPokedex pokedex;
+
+    IPokemonFactory pokemonFactory;
+
+    IPokemonMetadataProvider  metadataProvider;
+
+    @Before
+    public void initTestPokedexFactory() {
+
+        pokedexFactory =  Mockito.mock( IPokedexFactory.class );
+        pokedex =  Mockito.mock( IPokedex.class );
+        pokemonFactory =  Mockito.mock( IPokemonFactory.class );
+        metadataProvider =  Mockito.mock( IPokemonMetadataProvider.class );
+
+    }
+
+    @Test
+    public void testCreatePokedex() {
+
+        Mockito.when(pokedexFactory.createPokedex(metadataProvider, pokemonFactory)).thenReturn(pokedex);
+
+        assert(pokedexFactory.createPokedex(metadataProvider, pokemonFactory) == pokedex);
+
+    }
 }
