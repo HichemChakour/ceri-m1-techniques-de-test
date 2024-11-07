4e8852f8bee4f9c1e9df465db2a7d213d0062ec3
hchakour2003 <hchakour2003@gmail.com>
2024-11-07 09:58:11 +0100

    cover de PokedexException

--- src/test/java/fr/univavignon/pokedex/api/PokedexExceptionTest.java
+++ src/test/java/fr/univavignon/pokedex/api/PokedexExceptionTest.java
@@ -9,26 +9,22 @@ public class PokedexExceptionTest {
 
     @Test
     public void testPokedexExceptionMessage() {
-        // Le message d'erreur que nous voulons tester
+
         String errorMessage = "Test error message";
 
-        // Création de l'exception avec le message d'erreur
         PokedexException exception = new PokedexException(errorMessage);
 
-        // Vérification que le message est bien passé au constructeur de Exception
         assertEquals(errorMessage, exception.getMessage());
     }
 
     @Test
     public void testPokedexExceptionThrown() {
-        // Vérifie que l'exception est bien lancée avec le bon message
         String errorMessage = "Another test error message";
 
         Exception exception = assertThrows(PokedexException.class, () -> {
             throw new PokedexException(errorMessage);
         });
 
-        // Vérifie que le message d'erreur correspond bien à ce qui a été passé dans le constructeur
         assertEquals(errorMessage, exception.getMessage());
     }
 }
